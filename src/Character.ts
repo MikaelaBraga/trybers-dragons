34057fb728c8bd12215f801d46c44a5f914b4e08
MIkaela Braga <bragamikaela@gmail.com>
2022-03-27 18:10:15 -0300

    Inicia implementaçao da classe Character, mas ainda sem finalizar

--- src/Character.ts
+++ src/Character.ts
@@ -1,28 +1,31 @@
-import Archetype, { Mage } from './Archetypes';
-import Energy from './Energy';
-import Fighter from './Fighter';
-import Race, { Elf } from './Races';
+// import Archetype, { Mage } from './Archetypes';
+// import Energy from './Energy';
+// import Fighter from './Fighter';
+// import Race, { Elf } from './Races';
+// import getRandomInt from './utils';
 
-class Chararter implements Fighter {
-  private readonly _race: Race;
-  private readonly _archetype: Archetype;
-  private readonly _maxLifePoints: number;
-  private readonly _lifePoints: number;
-  private readonly _strength: number;
-  private readonly _defense: number;
-  private readonly _dexterity: number;
-  private readonly _energy: Energy;
+// ESSA TÁ MEIO DIFÍCIL ='(
 
-  constructor (name: string) {
-    this._race = new Elf(name, ); // deve receber como segundo parâmetro metade do maxLifePoints -> estudar mais
-    this._archetype = new Mage(name);
-    this._maxLifePoints =
-    this._lifePoints =
-    this._strength = 
-    this._defense = 
-    this._dexterity =
-    this._energy =
-  }
-}
+// class Chararter implements Fighter {
+//   private readonly _race: Race;
+//   private readonly _archetype: Archetype;
+//   private readonly _maxLifePoints: number;
+//   private readonly _lifePoints: number;
+//   private readonly _strength: number;
+//   private readonly _defense: number;
+//   private readonly _dexterity: number;
+//   private readonly _energy: Energy;
 
-export default Chararter;
+//   constructor (name: string) {
+//     this._race = new Elf(name, ); // deve receber como segundo parâmetro metade do maxLifePoints
+//     this._archetype = new Mage(name);
+//     this._maxLifePoints =
+//     this._lifePoints =
+//     this._strength = getRandomInt(1, 10);
+//     this._defense = getRandomInt(1, 10);
+//     this._dexterity =
+//     this._energy =
+//   }
+// }
+
+// export default Chararter;
