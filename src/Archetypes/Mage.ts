f5d1b8a25e0af0b7b8b8068e87be8ec65de78292
MIkaela Braga <bragamikaela@gmail.com>
2022-03-26 21:18:54 -0300

    Implementa classe Mage

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,18 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Mage extends Archetype {
+  private _energyType: EnergyType = 'mana' ;
+  private static _archetypesInstances = 0;
+
+  static createdArchetypeInstances(): number {
+    this._archetypesInstances += 1;
+    return this._archetypesInstances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Mage;
